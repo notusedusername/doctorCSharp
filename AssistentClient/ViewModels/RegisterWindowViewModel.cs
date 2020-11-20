4f56a36745e48e5ce5fb38afad81b7470e30e876
zsoltvarga0702 <zsoltvarga0702@yahoo.com>
2020-11-20 21:39:27 +0100

    AssitentClient change to ViewModel

---
+++ AssistentClient/ViewModels/RegisterWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.PlatformUI;
+using System.Windows;
+using GalaSoft.MvvmLight.Command;
+
+namespace AssistentClient.ViewModels
+{
+    public class RegisterWindowViewModel
+    {
+        public RegisterWindowViewModel()
+        {
+           // RegisterCommand = new DelegateCommand(Register);
+            this.BackCommand = new RelayCommand<Window>(this.BackButton);
+
+        }
+        public ICommand RegisterCommand { get; }
+        public ICommand BackCommand { get; }
+        public string Name { get; set; }
+        public string Taj { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public MainWindow m { get; set; }
+
+        //private void Register() => RegisterButton();
+        //private void Back() => BackButton();
+
+        private void BackButton(Window window)
+        {
+            if (window != null)
+            {
+                m.Top = window.Top;
+                m.Left = window.Left;
+                m.Show();
+                window.Close();
+            }
+        }
+    }
+}
